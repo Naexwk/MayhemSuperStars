ac33fca0c532e652ba7d5bd2d26c2814560b24bb
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-14 10:56:57 -0600

    KeepsWorking

--- Assets/Scripts/Managers/NametagController.cs
+++ Assets/Scripts/Managers/NametagController.cs
@@ -8,15 +8,17 @@ public class NametagController : MonoBehaviour
 {
     [SerializeField] private TMP_Text nametagname;
     private GameObject[] player;
+    private GameManager gameManager;
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-       if (scene.name == "SampleScene" && player != null) {
+        gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+        if (scene.name == "SampleScene" && player != null) {
             Camera mainCamera = Camera.main;
             Canvas canvas = GetComponent<Canvas>();
             canvas.worldCamera = mainCamera;
             foreach(GameObject players in player){
-                nametagname.text = players.GetComponent<PlayerController>().playerNumber.ToString();
-                Debug.Log(nametagname.text = players.GetComponent<PlayerController>().playerNumber.ToString());
+                nametagname.text = gameManager.networkPlayerNames[(int)players.GetComponent<PlayerController>().playerNumber].ToString();
+                Debug.Log(players.GetComponent<PlayerController>().playerNumber.ToString());
             }
             nametagname.color = new Color(0.5f,1f,0.5f,1f);
         }
