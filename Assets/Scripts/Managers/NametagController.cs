b5a5bb0c5c35fbc45c03c1764f0e0f97d49f2c82
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-14 10:43:13 -0600

    WorksForSomeReason

--- Assets/Scripts/Managers/NametagController.cs
+++ Assets/Scripts/Managers/NametagController.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class NametagController : MonoBehaviour
 {
-   void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    [SerializeField] private TMP_Text nametagname;
+    private GameObject[] player;
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-       if (scene.name == "SampleScene") {
-            Debug.Log("SAMPLE SCENE LOADED");
+       if (scene.name == "SampleScene" && player != null) {
             Camera mainCamera = Camera.main;
-            Debug.Log("MAIN CAMERA" + mainCamera);
             Canvas canvas = GetComponent<Canvas>();
             canvas.worldCamera = mainCamera;
-            Debug.Log("World Camera" + canvas.worldCamera);
+            foreach(GameObject players in player){
+                nametagname.text = players.GetComponent<PlayerController>().playerNumber.ToString();
+                Debug.Log(nametagname.text = players.GetComponent<PlayerController>().playerNumber.ToString());
+            }
+            nametagname.color = new Color(0.5f,1f,0.5f,1f);
         }
     }
-     private void Awake() {
+    private void Awake() {
+        player = GameObject.FindGameObjectsWithTag("Player");
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 }
